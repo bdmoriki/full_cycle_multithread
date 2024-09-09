59850ed0ad92795b894a833dd1edbaf9cbcbf672
Bruno <dupim1@hotmail.com>
2024-09-08 21:26:15 -0300

    Adicionando aula de mutex

--- 4-mutex/main.go
+++ 4-mutex/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var numeroChamadas uint64 = 0
 
 func main() {
+	// m := sync.Mutex{}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		numeroChamadas++
+		// m.Lock()
+		// numeroChamadas++
+		// m.Unlock()
+
+		atomic.AddUint64(&numeroChamadas, 1)
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("Você é o visitante número %d", numeroChamadas)))
 	})
